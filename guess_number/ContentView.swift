86b037a4ecdee78ab3f90a30ff78299cb115d6b7
Patty <L224749391@gmail.com>
2020-12-08 17:54:07 +0800

    Initial Commit

---
+++ guess_number/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guess_number
+//
+//  Created by 林湘羚 on 2020/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ guess_number/guess_numberApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_numberApp.swift
+//  guess_number
+//
+//  Created by 林湘羚 on 2020/12/8.
+//
+
+import SwiftUI
+
+@main
+struct guess_numberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
